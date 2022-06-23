2816e38810d9352eeb85cc583c483d4db967264c
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-23 19:12:23 +0300

    added a grid for activities

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ if(navigator.geolocation){
         .then(currentWeatherData=>{
             //console.log(currentWeatherData)
             displayCurrentWeather(currentWeatherData);
+            
         })
     })
     
@@ -29,12 +30,29 @@ function displayCurrentWeather(weatherData){
     //units conversion from default givenfrom the API;
     const tempInDegree = Math.round(weatherData.main.temp -  273.15, 2);
     const speedInKnots = Math.floor(weatherData.wind.speed * 1.944)
+
     //insert contents into the tags
     countryAndCity.textContent = `City/ Town: ${weatherData.name} ${weatherData.sys.country}`;
     currentIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
     weatherDescription.innerHTML = `${weatherData.weather[0].main} <br>${weatherData.weather[0].description}`;
     temperature.textContent = `${tempInDegree}°C`;
     windSpeed.textContent = `${speedInKnots} Knots`;
+
+    //conditions for displaying activities
+
+    // switch(tempInDegree){
+    //     case 21:
+    //         fetchSunnyActivities();
+    //     case 12:
+    //         fetchRainyActivities()
+            
+    // }
+    if (tempInDegree > 25){
+        fetchRainyActivities()
+    }else{
+        fetchSunnyActivities()
+    }
+    
 }
 
 //get location of the user input
@@ -67,3 +85,98 @@ searchForm.addEventListener('submit', e=>{
             })
     }
 })
+
+
+function fetchRainyActivities(){
+    fetch("http://localhost:3000/activities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            displayRainyActivities(fetchedData)
+            // displaySunnyActivities(fetchedData)
+            // displaySnowyActivities(fetchedData)
+            //console.log(fetchedData)
+        })
+}
+
+function fetchSunnyActivities(){
+    fetch("http://localhost:3000/activities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            //displayRainyActivities(fetchedData)
+            displaySunnyActivities(fetchedData)
+            //displaySnowyActivities(fetchedData)
+            //console.log(fetchedData)
+        })
+}
+
+function fetchSnowyActivities(){
+    fetch("http://localhost:3000/activities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            // displayRainyActivities(fetchedData)
+            // displaySunnyActivities(fetchedData)
+            displaySnowyActivities(fetchedData)
+            //console.log(fetchedData)
+        })
+}
+//grab the section to display the activtiy cards
+const cardSection = document.getElementById('card-section');
+
+//displays the rainy activities
+function displayRainyActivities(fetchedData){
+    const rainyActivities = fetchedData.rainyActivities;
+
+    rainyActivities.forEach(rainyActivity => {
+        let activityName = rainyActivity.name;
+        let imageUrl = rainyActivity.image;
+        //creates a div element to display the data
+        let cardItem = document.createElement('div')
+        //cardItem.setAttribute('class', 'card')
+        cardItem.innerHTML= `<div class="card">
+        <img id="acivity-image" src="${imageUrl}" alt=""/>
+        <div class="activity-description">${activityName}</div>
+        <button class="btn">Remove</button>
+        </div>`
+        cardSection.appendChild(cardItem)
+    });
+    
+}
+
+//displays the snow activities
+function displaySnowyActivities(fetchedData){
+    const snowActivities = fetchedData.snowActivities;
+
+    snowActivities.forEach(snowActivity=>{
+        let activityName = snowActivity.name;
+        let imageUrl = snowActivity.image;
+        //creates a div element to display the information
+        let cardItem = document.createElement('div');
+        cardItem.innerHTML= `<div class="card">
+        <img id="acivity-image" src="${imageUrl}" alt=""/>
+        <div class="activity-description">${activityName}</div>
+        <button class="btn">Remove</button>
+        </div>`
+        cardSection.appendChild(cardItem)
+
+    })
+    
+}
+
+//display the sunny activities
+function displaySunnyActivities(fetchedData){
+    const sunnyActivities = fetchedData.sunnyActivities;
+
+    sunnyActivities.forEach(sunnyActivity=>{
+        let activityName = sunnyActivity.name;
+        let imageUrl = sunnyActivity.image;
+        //creates a div element to display the information
+        let cardItem = document.createElement('div');
+        cardItem.innerHTML= `<div class="card">
+        <img id="acivity-image" src="${imageUrl}" alt=""/>
+        <div class="activity-description">${activityName}</div>
+        <button class="btn">Remove</button>
+        </div>`
+        cardSection.appendChild(cardItem)
+    })
+    
+}
