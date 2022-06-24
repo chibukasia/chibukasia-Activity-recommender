e45aa9664cfb4a29697f5b6a19d4b7779d1ec72d
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-24 17:47:53 +0300

    changed the search results to reflect activities

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ if(navigator.geolocation){
     })
     
 }
+
 //get requests
 function fetchRainyActivities(){
     fetch("http://localhost:3000/rainyActivities")
@@ -30,7 +31,9 @@ function fetchSunnyActivities(){
     fetch("http://localhost:3000/sunnyActivities")
         .then(res=>res.json())
         .then(fetchedData=>{
-            displaySunnyActivities(fetchedData)
+            let newData = new Set(fetchedData)
+            displaySunnyActivities(newData)
+            
         })
 }
 
@@ -38,7 +41,26 @@ function fetchSnowyActivities(){
     fetch("http://localhost:3000/snowActivities")
         .then(res=>res.json())
         .then(fetchedData=>{
-            displaySnowyActivities(fetchedData)
+            let newData = new Set(fetchedData)
+            displaySnowyActivities(newData)
+        })
+}
+
+function fetchSnowyAndRainyNightActivities(){
+    fetch("http://localhost:3000/rainyAndSnowNightActivities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            let newData = new Set(fetchedData)
+            displaySnowyAndRainyNightActivitie(newData)
+        })
+}
+
+function fetchNormalNightActivities(){
+    fetch("http://localhost:3000/normalNightActivities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            let newData = new Set(fetchedData)
+            displayNormalNightActivities(newData)
         })
 }
 
@@ -84,7 +106,38 @@ function patchSnowyActivities(activityObject){
             console.log(data)
         })
 }
+
+function patchSnowyAndRainyNightActivities(activityObject){
+    fetch(`http://localhost:3000/rainyAndSnowNightActivities/${activityObject.id}`,{
+        method: "PATCH",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(activityObject)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+}
+
+function patchNormalNightActivities(activityObject){
+    fetch(`http://localhost:3000/normalNightActivities/${activityObject.id}`,{
+        method: "PATCH",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(activityObject)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+}
+
 //function to display weather data
+
+const cardSection = document.getElementById('card-section');
 function displayCurrentWeather(weatherData){
     const countryAndCity = document.getElementById('city-country');
     const currentIcon = document.querySelector('#currentIcon');
@@ -97,23 +150,42 @@ function displayCurrentWeather(weatherData){
     const tempInDegree = Math.round(weatherData.main.temp -  273.15, 2);
     const speedInKnots = Math.floor(weatherData.wind.speed * 1.944)
 
+    let iconId = weatherData.weather[0].icon
+    console.log(typeof iconId)
+
     //insert contents into the tags
     countryAndCity.textContent = `City/ Town: ${weatherData.name} ${weatherData.sys.country}`;
-    currentIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    currentIcon.src = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
     weatherDescription.innerHTML = `${weatherData.weather[0].main} <br>${weatherData.weather[0].description}`;
     temperature.textContent = `${tempInDegree}°C`;
     windSpeed.textContent = `${speedInKnots} Knots`;
 
+    //Display acitivities based on the weather conditions
     
-    if (tempInDegree < 25 && tempInDegree > 10){
-        fetchRainyActivities()
-
-    }else if(tempInDegree < 10){
-        fetchSnowyActivities()
-    }else if(tempInDegree > 25){
-        fetchSunnyActivities()
+    if(iconId==="01n" || iconId==="02n" || iconId==="03n" ){
+        cardSection.innerHTML=``
+        fetchNormalNightActivities()
+    }else if(iconId==="04n" || iconId==="09n" || iconId==="10n" || iconId==="11n" || iconId==="13n" || iconId==="50n"){
+        cardSection.innerHTML=``
+        fetchSnowyAndRainyNightActivities();
+    }else{
+        if (weatherData.weather[0].main==="Clear" || weatherData.weather[0].description==="scattered clouds"|| weatherData.weather[0].description==="few clouds"){
+            cardSection.innerHTML=``
+            fetchSunnyActivities()
+        }else if(weatherData.weather[0].main==="Clouds" &&(weatherData.weather[0].description !="scattered clouds"|| weatherData.weather[0].description !="few clouds")){
+            cardSection.innerHTML=``
+            fetchRainyActivities()
+        }else if(weatherData.weather[0].main==="Rain" || weatherData.weather[0].main==="Drizzle" || weatherData.weather[0].main==="Thunderstorm"){
+            cardSection.innerHTML=``
+            fetchRainyActivities()
+        }else if(weatherData.weather[0].main==="Snow" ){
+            cardSection.innerHTML=``
+            fetchSnowyActivities()
+        }else if(weatherData.weather[0].main==="Clear"){
+            cardSection.innerHTML=``
+            fetchSunnyActivities()
+        }
     }
-    
 }
 
 //get location of the user input
@@ -139,14 +211,9 @@ searchForm.addEventListener('submit', e=>{
     }
 })
 
-
-
-//grab the section to display the activtiy cards
-const cardSection = document.getElementById('card-section');
-//const actionList = document.querySelector('#action-list');
-
 //displays the rainy activities
 function displayRainyActivities(rainyActivities){
+    
     rainyActivities.forEach(rainyActivity => {
         let activityName = rainyActivity.name;
         let imageUrl = rainyActivity.image;
@@ -205,9 +272,9 @@ function displaySnowyActivities(snowActivities){
         //grab the delete and like buttons 
         let deleteBtn = cardItem.querySelector("#remove");
         let likeBtn = cardItem.querySelector("#like-btn");
-        cardSection.appendChild(cardItem)
 
         cardSection.appendChild(cardItem)
+
         deleteBtn.addEventListener('click', e=>{
             if (confirm('Are you sure you want to delete?')){
                 cardItem.remove()
@@ -244,9 +311,10 @@ function displaySunnyActivities(sunnyActivities){
         //grab the delete and like buttons 
         let deleteBtn = cardItem.querySelector("#remove");
         let likeBtn = cardItem.querySelector("#like-btn");
-        cardSection.appendChild(cardItem)
 
+        //cardSection.appendChild(cardItem)
         cardSection.appendChild(cardItem)
+        
         deleteBtn.addEventListener('click', e=>{
             if (confirm('Are you sure you want to delete?')){
                 cardItem.remove()
@@ -261,6 +329,80 @@ function displaySunnyActivities(sunnyActivities){
         })
     })
     
+}
+
+//display snowy and rainy night activities
+function displaySnowyAndRainyNightActivitie(snowyAndRainyActivities){
+    snowyAndRainyActivities.forEach(activity=>{
+        let likes = activity.likes;
+        let activityName = activity.name;
+        let imageUrl = activity.image;
+        //creates a div element to display the information
+        let cardItem = document.createElement('div');
+        cardItem.innerHTML= `<div class="card">
+                            <img id="acivity-image" src="${imageUrl}" alt=""/>
+                            <div class="activity-description">${activityName}</div>
+                            <p id="likes">${likes} Favorites</p>
+                            <button class="btn" id="remove">Remove</button>
+                            <button class="btn" id='like-btn' >Like ❤️</button>
+                            </div>`
+
+        //grab the delete and like buttons 
+        let deleteBtn = cardItem.querySelector("#remove");
+        let likeBtn = cardItem.querySelector("#like-btn");
+        cardSection.appendChild(cardItem)
+
+        deleteBtn.addEventListener('click', e=>{
+            if (confirm('Are you sure you want to delete?')){
+                cardItem.remove()
+            }
+        });
+        let likesCount = cardItem.querySelector("#likes");
+        likeBtn.addEventListener('click', e=>{
+            likes += 1;
+            likesCount.textContent = `${likes} Favorites`
+            activity.likes = likes;
+            patchSnowyAndRainyNightActivities(activity)
+        })
+
+    })
+}
+
+//display normal night activiies
+function displayNormalNightActivities(nightActivities){
+    nightActivities.forEach(activity=>{
+        let likes = activity.likes;
+        let activityName = activity.name;
+        let imageUrl = activity.image;
+        //creates a div element to display the information
+        let cardItem = document.createElement('div');
+        cardItem.innerHTML= `<div class="card">
+                            <img id="acivity-image" src="${imageUrl}" alt=""/>
+                            <div class="activity-description">${activityName}</div>
+                            <p id="likes">${likes} Favorites</p>
+                            <button class="btn" id="remove">Remove</button>
+                            <button class="btn" id='like-btn' >Like ❤️</button>
+                            </div>`
+
+        //grab the delete and like buttons 
+        let deleteBtn = cardItem.querySelector("#remove");
+        let likeBtn = cardItem.querySelector("#like-btn");
+        cardSection.appendChild(cardItem)
+
+        deleteBtn.addEventListener('click', e=>{
+            if (confirm('Are you sure you want to delete?')){
+                cardItem.remove()
+            }
+        });
+        let likesCount = cardItem.querySelector("#likes");
+        likeBtn.addEventListener('click', e=>{
+            likes += 1;
+            likesCount.textContent = `${likes} Favorites`
+            activity.likes = likes;
+            patchNormalNightActivities(activity)
+        })
+
+    })
 }
 
 
