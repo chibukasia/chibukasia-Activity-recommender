1f744753e44823c4f78f625cf294126f6a6e408b
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-24 09:17:27 +0300

    added the night activities in db.json

--- src/index.js
+++ src/index.js
@@ -11,12 +11,78 @@ if(navigator.geolocation){
         fetch(api)
         .then(response=>response.json())
         .then(currentWeatherData=>{
-            //console.log(currentWeatherData)
             displayCurrentWeather(currentWeatherData);
             
         })
     })
     
+}
+//get requests
+function fetchRainyActivities(){
+    fetch("http://localhost:3000/rainyActivities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            displayRainyActivities(fetchedData)
+        })
+}
+
+function fetchSunnyActivities(){
+    fetch("http://localhost:3000/sunnyActivities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            displaySunnyActivities(fetchedData)
+        })
+}
+
+function fetchSnowyActivities(){
+    fetch("http://localhost:3000/snowActivities")
+        .then(res=>res.json())
+        .then(fetchedData=>{
+            displaySnowyActivities(fetchedData)
+        })
+}
+
+//perform a pacth request
+function patchRainyActivities(activityObject){
+    fetch(`http://localhost:3000/rainyActivities/${activityObject.id}`,{
+        method: "PATCH",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(activityObject)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+}
+
+function patchSunnyActivities(activityObject){
+    fetch(`http://localhost:3000/sunnyActivities/${activityObject.id}`,{
+        method: "PATCH",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(activityObject)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+}
+
+function patchSnowyActivities(activityObject){
+    fetch(`http://localhost:3000/snowActivities/${activityObject.id}`,{
+        method: "PATCH",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(activityObject)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
 }
 //function to display weather data
 function displayCurrentWeather(weatherData){
@@ -38,18 +104,13 @@ function displayCurrentWeather(weatherData){
     temperature.textContent = `${tempInDegree}°C`;
     windSpeed.textContent = `${speedInKnots} Knots`;
 
-    //conditions for displaying activities
-
-    // switch(tempInDegree){
-    //     case 21:
-    //         fetchSunnyActivities();
-    //     case 12:
-    //         fetchRainyActivities()
-            
-    // }
-    if (tempInDegree > 25){
+    
+    if (tempInDegree < 25 && tempInDegree > 10){
         fetchRainyActivities()
-    }else{
+
+    }else if(tempInDegree < 10){
+        fetchSnowyActivities()
+    }else if(tempInDegree > 25){
         fetchSunnyActivities()
     }
     
@@ -71,112 +132,135 @@ searchForm.addEventListener('submit', e=>{
             .then(response=>response.json())
             .then(weatherData=>{
                 displayCurrentWeather(weatherData)
-                //console.log(weatherData)
             })
-            .catch((error)=>{
-                //document.body.innerHTML = `<h1> Innvalid name for City or Town...refreshing in 3 seconds</h1>`;
-                alert("Invalid name for City or Town. Please enter the a valid name")
-                // setTimeout(function() {
-                    
-                //     location.reload('true')
-                //   }, 3000)
-                  
-                
+            .catch(()=>{
+                alert("Invalid name for City or Town. Please enter the a valid name")  
             })
     }
 })
 
 
-function fetchRainyActivities(){
-    fetch("http://localhost:3000/activities")
-        .then(res=>res.json())
-        .then(fetchedData=>{
-            displayRainyActivities(fetchedData)
-            // displaySunnyActivities(fetchedData)
-            // displaySnowyActivities(fetchedData)
-            //console.log(fetchedData)
-        })
-}
 
-function fetchSunnyActivities(){
-    fetch("http://localhost:3000/activities")
-        .then(res=>res.json())
-        .then(fetchedData=>{
-            //displayRainyActivities(fetchedData)
-            displaySunnyActivities(fetchedData)
-            //displaySnowyActivities(fetchedData)
-            //console.log(fetchedData)
-        })
-}
-
-function fetchSnowyActivities(){
-    fetch("http://localhost:3000/activities")
-        .then(res=>res.json())
-        .then(fetchedData=>{
-            // displayRainyActivities(fetchedData)
-            // displaySunnyActivities(fetchedData)
-            displaySnowyActivities(fetchedData)
-            //console.log(fetchedData)
-        })
-}
 //grab the section to display the activtiy cards
 const cardSection = document.getElementById('card-section');
+//const actionList = document.querySelector('#action-list');
 
 //displays the rainy activities
-function displayRainyActivities(fetchedData){
-    const rainyActivities = fetchedData.rainyActivities;
-
+function displayRainyActivities(rainyActivities){
     rainyActivities.forEach(rainyActivity => {
         let activityName = rainyActivity.name;
         let imageUrl = rainyActivity.image;
+        let likes = rainyActivity.likes;
         //creates a div element to display the data
         let cardItem = document.createElement('div')
         //cardItem.setAttribute('class', 'card')
         cardItem.innerHTML= `<div class="card">
-        <img id="acivity-image" src="${imageUrl}" alt=""/>
-        <div class="activity-description">${activityName}</div>
-        <button class="btn">Remove</button>
-        </div>`
+                            <img id="acivity-image" src="${imageUrl}" alt=""/>
+                            <div class="activity-description">${activityName}</div>
+                            <p id="likes">${likes} Favorites</p>
+                            <button class="btn" id="remove">Remove</button>
+                            <button class="btn" id='like-btn' >Like ❤️</button>
+                            </div>`
+
+        //grab the delete and like buttons 
+        let deleteBtn = cardItem.querySelector("#remove");
+        let likeBtn = cardItem.querySelector("#like-btn");
+        //append card to the html div node
         cardSection.appendChild(cardItem)
+        //add event listener to delete and like button
+        deleteBtn.addEventListener('click', e=>{
+            if (confirm('Are you sure you want to delete?')){
+                cardItem.remove()
+            }
+            
+        });
+        let likesCount = cardItem.querySelector("#likes");
+        likeBtn.addEventListener('click', e=>{
+            likes += 1;
+            likesCount.textContent = `${likes} Favorites`
+            rainyActivity.likes = likes;
+            patchRainyActivities(rainyActivity)
+        })
+
     });
     
 }
 
 //displays the snow activities
-function displaySnowyActivities(fetchedData){
-    const snowActivities = fetchedData.snowActivities;
+function displaySnowyActivities(snowActivities){
 
     snowActivities.forEach(snowActivity=>{
         let activityName = snowActivity.name;
         let imageUrl = snowActivity.image;
+        let likes = snowActivity.likes;
         //creates a div element to display the information
         let cardItem = document.createElement('div');
         cardItem.innerHTML= `<div class="card">
-        <img id="acivity-image" src="${imageUrl}" alt=""/>
-        <div class="activity-description">${activityName}</div>
-        <button class="btn">Remove</button>
-        </div>`
+                            <img id="acivity-image" src="${imageUrl}" alt=""/>
+                            <div class="activity-description">${activityName}</div>
+                            <p id="likes">${likes} Favorites</p>
+                            <button class="btn" id="remove">Remove</button>
+                            <button class="btn" id='like-btn' >Like ❤️</button>
+                            </div>`
+        //grab the delete and like buttons 
+        let deleteBtn = cardItem.querySelector("#remove");
+        let likeBtn = cardItem.querySelector("#like-btn");
         cardSection.appendChild(cardItem)
+
+        cardSection.appendChild(cardItem)
+        deleteBtn.addEventListener('click', e=>{
+            if (confirm('Are you sure you want to delete?')){
+                cardItem.remove()
+            }
+        });
+        let likesCount = cardItem.querySelector("#likes");
+        likeBtn.addEventListener('click', e=>{
+            likes += 1;
+            likesCount.textContent = `${likes} Favorites`
+            snowActivity.likes = likes;
+            patchSnowyActivities(snowActivity)
+        })
 
     })
     
 }
 
 //display the sunny activities
-function displaySunnyActivities(fetchedData){
-    const sunnyActivities = fetchedData.sunnyActivities;
+function displaySunnyActivities(sunnyActivities){
 
     sunnyActivities.forEach(sunnyActivity=>{
+        let likes = sunnyActivity.likes;
         let activityName = sunnyActivity.name;
         let imageUrl = sunnyActivity.image;
         //creates a div element to display the information
         let cardItem = document.createElement('div');
         cardItem.innerHTML= `<div class="card">
-        <img id="acivity-image" src="${imageUrl}" alt=""/>
-        <div class="activity-description">${activityName}</div>
-        <button class="btn">Remove</button>
-        </div>`
+                            <img id="acivity-image" src="${imageUrl}" alt=""/>
+                            <div class="activity-description">${activityName}</div>
+                            <p id="likes">${likes} Favorites</p>
+                            <button class="btn" id="remove">Remove</button>
+                            <button class="btn" id='like-btn' >Like ❤️</button>
+                            </div>`
+        //grab the delete and like buttons 
+        let deleteBtn = cardItem.querySelector("#remove");
+        let likeBtn = cardItem.querySelector("#like-btn");
         cardSection.appendChild(cardItem)
+
+        cardSection.appendChild(cardItem)
+        deleteBtn.addEventListener('click', e=>{
+            if (confirm('Are you sure you want to delete?')){
+                cardItem.remove()
+            }
+        });
+        let likesCount = cardItem.querySelector("#likes");
+        likeBtn.addEventListener('click', e=>{
+            likes += 1;
+            likesCount.textContent = `${likes} Favorites`
+            sunnyActivity.likes = likes;
+            patchSunnyActivities(sunnyActivity)
+        })
     })
     
 }
+
+
